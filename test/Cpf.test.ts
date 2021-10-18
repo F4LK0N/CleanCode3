9ebf937a6fd6cff8af60ef07fc641fd3b77fe711
F4LK0N <otaviosoria@gmail.com>
2021-10-17 23:38:56 -0300

    Tests Naming

--- test/Cpf.test.ts
+++ test/Cpf.test.ts
@@ -1,10 +1,10 @@
 import Cpf from "../src/Cpf"
 
-test("Deve validar um cpf", function () {
+test("CPF - Valid", function () {
     const cpf = new Cpf("847.903.332-05");
     expect(cpf.value).toBe("847.903.332-05");
 });
 
-test("Não deve validar um cpf", function () {
+test("CPF - Invalid", function () {
     expect(() => new Cpf("111.111.111-11")).toThrow(new Error("Invalid cpf"));
 });
