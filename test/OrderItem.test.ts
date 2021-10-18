a8189627b267a2f1eb8cf77d3b9d31175547ec84
F4LK0N <otaviosoria@gmail.com>
2021-10-17 23:45:17 -0300

    Tests Naming

--- test/OrderItem.test.ts
+++ test/OrderItem.test.ts
@@ -1,6 +1,6 @@
 import OrderItem from "../src/OrderItem";
 
-test("Deve criar um item de pedido", function () {
+test("OrderItem - Create", function () {
     const orderItem = new OrderItem(1, 1000, 2);
     expect(orderItem.getTotal()).toBe(2000);
 });
