9d8a27834f0f6aaa3503f9974bafe348645f7938
F4LK0N <otaviosoria@gmail.com>
2021-10-18 23:33:18 -0300

    Product - Body

--- src/Product.ts
+++ src/Product.ts
@@ -1,28 +1,22 @@
+import Body from "./Body";
+
 export default class Product
 {
     id: number;
     idCategory: number;
     name: string;
-
     price: number;  //cents
-
-    width: number;  //cm
-    height: number; //cm
-    length: number; //cm
-    weight: number; //grams
+    body: Body;
 
 
 
-    constructor (id: number, idCategory: number, name: string, price: number, width: number=0, height: number=0, length: number=0, weight: number=0)
+    constructor (id: number, idCategory: number, name: string, price: number, body: Body = new Body())
     {
         this.id = id;
         this.idCategory = idCategory;
         this.name = name;
         this.price = price;
-        this.width = width;
-        this.height = height;
-        this.length = length;
-        this.weight = weight;
+        this.body = body;
     }
 
 }
---
+++ test/Product.test.ts
@@ -0,0 +1,12 @@
+import Product from "../src/Product";
+import Body from "../src/Body";
+
+test("Product - Create", function () {
+    const product = new Product(1, 1, "Name", 1000);
+    expect(product).toBeDefined();
+});
+
+test("Product - Volume", function () {
+    const product = new Product(1, 1, "Name", 1000, new Body(10, 12, 20));
+    expect(product.body.volume).toBe(2400);
+});
