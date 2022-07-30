d125868c7b549af1bbafecc761ffcd65da7d91d7
OtavioSoria <devel_17@autoavaliar.com.br>
2022-07-30 04:28:31 -0300

    Body Volume

--- app/Body.php
+++ app/Body.php
@@ -2,17 +2,19 @@
 
 class Body {
 
-    private float $weight = 0.0; //cm
+    private float $width  = 0.0; //cm
     private float $height = 0.0; //cm
     private float $length = 0.0; //cm
     private float $volume = 0.0; //m3
 
+    private float $weight = 0.0;
     private float $density = 0.0;
 
 
-    public function __construct(float $weight = 0, float $height = 0, float $length = 0)
+
+    public function __construct(float $width = 0, float $height = 0, float $length = 0, float $weight=0)
     {
-        $this->weight = $weight;
+        $this->width = $width;
         $this->height = $height;
         $this->length = $length;
         $this->calculateVolume();
@@ -21,7 +23,7 @@ class Body {
 
     private function calculateVolume(): void
     {
-        $this->volume = ($this->weight * $this->height * $this->length) / 1000000.0;
+        $this->volume = ($this->width * $this->height * $this->length) / 1000000.0;
     }
 
     public function getVolume(): float
